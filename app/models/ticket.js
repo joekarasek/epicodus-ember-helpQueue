eefd23d0b855d87ba84548518bb9d75396284510
Anna Lawley and Joseph Karasek <alawley1@gmail.com>
2016-04-04 09:14:04 -0700

    add form to create ticket

--- app/models/ticket.js
+++ app/models/ticket.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  names: DS.attr(),
-  location: DS.attr(),
-  notes: DS.attr()
+  names: DS.attr('string'),
+  location: DS.attr('string'),
+  notes: DS.attr('string')
 });
